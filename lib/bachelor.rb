accc9de589de9f541e2b1516341ed0d5b73ff04b
Mei Wong <fongmeiw@gmail.com>
2019-10-11 16:57:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -51,8 +51,8 @@ end
 def get_average_age_for_season(data, season)
   total_age = 0
   
-  data[:season].each do |contestant|
-    total_age += contestant[:age].to_i
+  data["#{season}"].each do |contestant|
+    total_age += contestant["age"].to_i
   end
   average = total_age.to_f/data[:"#{season}"].length
   average.round
